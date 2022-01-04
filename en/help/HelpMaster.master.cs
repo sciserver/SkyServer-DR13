4837a3a64ebb7050e37c544796d0ea51530cdcd5
raddick <raddick@jhu.edu>
2022-01-04 16:37:33 -0500

    Fixed link to Glossary in left-hand menu of help section
    From develop branch commit 5f47f1bf40182ecc7f01cb224aed1b12b3596e62

--- en/help/HelpMaster.master.cs
+++ en/help/HelpMaster.master.cs
@@ -34,6 +34,7 @@ namespace SkyServer.Help
             gutter += "30.4,Projects,/help/docs/introduction.aspx#proj;";
 
             gutter += "10,FAQ," + globals.SdssUrl + "help/;";
+            gutter += "4,Glossary," + globals.SdssUrl + "help/glossary/;";
 
             gutter += "33,Cooking with Sloan,/help/cooking/;";
             gutter += "33.1,General,/help/cooking/general/;";
@@ -43,34 +44,33 @@ namespace SkyServer.Help
             gutter += "33.5,QSOs/Cosmology,/help/cooking/quasars/;";
             gutter += "33.6,Teaching/Labs,/help/cooking/labs/;";
 
-            //  gutter += "55,Tool User Guides,/tools/started;";
+            gutter += "55,Tool User Guides,/tools/started;";
             gutter += "13,Contact Help Desk," + globals.ContactUrl+"&url="+Server.UrlEncode("http://"+Request.ServerVariables["SERVER_NAME"]+Request.ServerVariables["SCRIPT_NAME"]+Request.ServerVariables["QUERY_STRING"]) + ";";
 
             gutter += "1,<hr />,;";
 
-            gutter += "85,<b>Searching SDSS</b>,/tools/search;";
+            gutter += "85,<b>Searching SDSS-III</b>,/tools/search;";
             gutter += "999,SQL Tutorial,/help/howto/search/;";
             gutter += "2,SQL in SkyServer,/help/docs/sql_help.aspx;";
-            gutter += "18,Sample SQL Queries,/help/docs/realquery.aspx;";
-            gutter += "14,Query Limits,/help/docs/limits.aspx;";
-            gutter += "50,Searching Advice,/help/cooking/advice.aspx;";
-
-            gutter += "1,<hr />,;";
             gutter += "11,About the Database,/help/docs/intro.aspx;";
             gutter += "16,Table Descriptions,/help/docs/tabledesc.aspx;";
+            gutter += "18,Sample SQL Queries,/help/docs/realquery.aspx;";
             gutter += "1,Schema Browser,/help/browser/browser.aspx;";
-            gutter += "4,Glossary," + globals.SdssUrl + "help/glossary/;";
-            gutter += "5,Algorithms," + globals.SdssUrl + "algorithms/;";
+            gutter += "50,Searching Advice,/help/cooking/advice.aspx;";
+            gutter += "14,Query Limits,/help/docs/limits.aspx;";
+            gutter += "1,<hr />,;";
 
-            //  gutter += "19,Data Publications,/sdss/pubs/;";
-            //  gutter += "19,<b>Details of the Data</b>,/sdss/pubs/;";
+            gutter += "19,<b>Details of the Data</b>,/sdss/pubs/;";
+            gutter += "5,Algorithms," + globals.SdssUrl + "algorithms/;";
+            gutter += "19,Data Publications,/sdss/pubs/;";
+
             //  gutter += "20,Downloads,/help/download/;";
             //  gutter += "20.1,sdssQA,/help/download/sdssQA/;";
             //  gutter += "20.2,sqlcl,/help/download/sqlcl/;"
             //  gutter += "20.3,Emacs interface,"+emacsURL+"/skyserver.el;";
             //  gutter += "20.4,Cooking with Sloan,/help/download/cooking/;"
 
-            gutter += "8,API/Tools,/help/docs/api.aspx;";
+            gutter += "8,API,/help/docs/api.aspx;";
 
             HelpGutter = new Gutter(imgpath, url, Response);
 
